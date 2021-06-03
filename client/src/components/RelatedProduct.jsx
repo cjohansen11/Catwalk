625cb79c964a6b2b162b0b335b1aecf6dedd6ffa
Christian Johansen <cjohansen11@me.com>
2021-06-03 17:10:59 -0400

    Added dummy data

--- client/src/components/RelatedProduct.jsx
+++ client/src/components/RelatedProduct.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+/* ** IMPORT TEST DATA ** */
+import dummy from '../../../dummy_data/related_product.js';
+
 const RelatedProduct = () => {
   return (
     <div>
+      {console.log(dummy)}
       <h2>Welcome to the Related Products section</h2>
       <h2>Welcome to the Your Outfit section</h2>
     </div>
