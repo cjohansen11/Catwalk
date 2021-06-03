01c7261ab6e9537f5268ed46b4664b416ad224b0
Christian Johansen <cjohansen11@me.com>
2021-06-03 14:09:48 -0400

    Created component for base level of related products component

---
+++ client/src/components/RelatedProduct.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+const RelatedProduct = (props) => {
+return (
+  <div>
+    <h2>Welcome to the Related Products section</h2>
+    <h2>Welcome to the Your Outfit section</h2>
+  </div>
+)
+}
