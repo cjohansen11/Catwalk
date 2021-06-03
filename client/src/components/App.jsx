1c705bedfa0cad97676fd2b82d4a1af950009bcd
Christian Johansen <cjohansen11@me.com>
2021-06-03 15:11:05 -0400

    Minor changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import styles from './app.css'
 
+/* ** IMPORT COMPONENT FILES ** */
+import RelatedProduct from './RelatedProduct.jsx';
+
 class App extends React.Component {
 	constructor (props) {
 		super(props)
@@ -10,7 +13,7 @@ class App extends React.Component {
 		return (
 			<div>
 				<h1 className={styles.testClass}>CATWALK</h1>
-				<RelatedProduct props={this.state} />
+				<RelatedProduct /*props={this.state}*/ />
 			</div>
 		);
 	}
