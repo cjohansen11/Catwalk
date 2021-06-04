a528a51064cfa6fb31b1ef0d703f5e0d70566bc6
Alec Stavros <alecwstavros@gmail.com>
2021-06-04 18:10:54 -0500

    added components, config.js, and a get req

---
+++ client/src/components/Overview/Imagegallery.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+class Imagegallery extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render () {
+    return (
+      <div>
+        <h4>Image Gallery</h4>
+
+      </div>
+    );
+  }
+}
+
+export default Imagegallery;
---
+++ client/src/components/Overview/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  TOKEN: 'aebd41cd494f42ad6512fc208c13af966bbfd959'
+};
