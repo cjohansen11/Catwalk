d6717b3a151d7b813cabb003c7acbe9177c53bc4
Christian Johansen <cjohansen11@me.com>
2021-06-09 10:47:57 -0400

    Added initial table set up for comparison modal

--- client/src/components/RelatedProducts/Modal.jsx
+++ client/src/components/RelatedProducts/Modal.jsx
@@ -5,8 +5,26 @@ import '../../styles/modal.css';
 const Modal = ({ setToggleModal }) => {
   return (
     <div className={`${'modal'}`} onClick={() => setToggleModal(false)}>
-      <button className={'button'} onClick={() => setToggleModal(false)}>Close</button>
-      <div className={`${'feature-table'}`}>This is a modal</div>
+      <div className={`${'feature-table'}`}>
+        <button className={'button'} onClick={() => setToggleModal(false)}>Close</button>
+        <h4>Comparing</h4>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="1">Feature Product Name</th>
+              <th colSpan="1"></th>
+              <th colSpan="1">Comparing Product Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>Features</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
