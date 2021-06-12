0e9a89345065d812a182b67252cfa60ac45dce1d
Christian Johansen <cjohansen11@me.com>
2021-06-12 15:41:19 -0400

    CSS styling on product cards

--- client/src/components/RelatedProducts/OutfitCard.jsx
+++ client/src/components/RelatedProducts/OutfitCard.jsx
@@ -23,7 +23,7 @@ const OutfitCard = ({ product, removeOutfit }) => {
   return (
     <div className={`${'productCard'} ${'outfit-card'}`}>
       <img className={'image'} src={product.styles.results[0].photos[0].thumbnail_url} alt="" className="previewImage"></img>
-      <h5 className={'productName'}>{product.details.name}</h5>
+      <h5 className={'productName'}>{product.details.name.toUpperCase()}</h5>
       <h5 className={'productCategory'}>{product.details.category}</h5>
       <StarRating ratings={ratings} />
       {defaultStyle.sale_price ? <span className={'productPrice'}><h5 className={'sale-price'}>{defaultStyle.sale_price}</h5><h5 className={'default-price'}>{defaultStyle.original_price}</h5></span> : <span className={'productPrice'}><h5>{defaultStyle.original_price}</h5></span>}      <div className={'actionButton'}
