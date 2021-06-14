98ea81a7c6ffb879165a44261a2ac1eecab1ad68
Alec Stavros <alecwstavros@gmail.com>
2021-06-14 10:40:11 -0500

    Revert "expanded view added"
    This reverts commit 4112d73a60cfbdf6ce4e9c42202096ecb989a8cc.

--- client/src/components/Overview/corosel.jsx
+++ client/src/components/Overview/corosel.jsx
@@ -2,63 +2,34 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import '../../styles/imageGallery.css';
-import ExpandedView from './ExpandedView.jsx';
+
 class ImageGallaryComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toggleExpanded: false,
-      currentUrl: '',
-    };
-
-    this.togglenotexpanded = this.togglenotexpanded.bind(this);
-  }
-
-
-  toggle(e, value, url) {
-    e.preventDefault();
-    console.log('this will run the modal');
-    this.setState({
-      toggleExpanded: value,
-      currentUrl: url,
-    });
-  }
-
-  togglenotexpanded(e, value) {
-    e.preventDefault();
-    this.setState({
-      toggleExpanded: value,
-    });
-  }
-
   render() {
     if (!this.props.currentStyle) {
       return <span>Loading...</span>;
     }
     return (
-      <div className ='galleryContainer'>
+      <div>
 
-        <Carousel autoPlay interval="5000" transitionTime="500" >
+        <Carousel autoPlay interval="5000" transitionTime="500">
 
 
           {this.props.currentStyle.photos.map(photo => {
-            return <div className='photodiv' key={photo.url} onClick={(event) => { this.toggle(event, true, photo.url); }}>
-
+            return <div className='photodiv' key={photo.url}>
               <img className='photo' src={photo.url} />
 
             </div>;
           })}
 
+
+
+
+
+
+
+
         </Carousel>
-
-        {this.state.toggleExpanded ? <ExpandedView
-          setToggle={this.togglenotexpanded}
-          photoUrl={this.state.currentUrl}
-        /> : null}
-
       </div>
-
-
     );
   }
 }
